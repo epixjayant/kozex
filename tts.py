ba136349a3d2d3f808b187fe292852f787513bda
jayant raj verma <84304081+epixjayant@users.noreply.github.com>
2025-03-22 19:34:12 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -4,6 +4,6 @@ import os
 def text_to_speech(text, output_file="output.mp3"):
     tts = gTTS(text=text, lang='en')
     tts.save(output_file)
-    os.system(f"start {output_file}")  # Play the audio file
+    os.system(f"open {output_file}")  # Play the audio file
 
 text_to_speech("Hello, welcome to the Prodigal AI Voice Cloning Hackathon!")
